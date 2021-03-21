d5a197ee41683934424bb94ec6913a983628d13e
Renault Pierrick <pierrick.renault@etudiant.univ-rennes1.fr>
2021-03-21 21:44:34 +0100

    Creating a Service tu Get Album Data

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
-import {Response} from '@angular/http';
+import {Http, Response} from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -10,7 +9,7 @@ export class ProductService {
   constructor(private _http: Http) {  }
 
   getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map(res => res.json());
+    return this._http.get(this._albumUrl).map(response => response.json());
   }
 }
 
