27c13b779c4ba0fef1e9a32f3b2956e7f380ef52
Renault Pierrick <pierrick.renault@etudiant.univ-rennes1.fr>
2021-03-21 21:35:59 +0100

    Creating a Service tu Get Album Data

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import '../product.service'
+import {ProductService} from '../product.service';
 
 @Component({
   selector: 'app-product-description',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
+  private albumInfo;
 
-  constructor() { }
+  constructor(private _productService: ProductService) {
+  }
 
   ngOnInit() {
+    this._productService.getAlbum(1);
+    this.subscribe();
+  }
+
+  subscribe() {
+    return this.albumInfo;
   }
 
 }
